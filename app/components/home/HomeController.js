c469da80f4fb74320324ca8347a0826271f1e505
daviinacio <aazz6850@gmail.com>
2019-09-19 10:07:22 -0300

    update HomeController.js

--- app/components/home/HomeController.js
+++ app/components/home/HomeController.js
@@ -1,4 +1,4 @@
 angular.module('myApp').controller('HomeController', ['$scope', '$location', function($scope, $location){
-    $scope.message = 'Hello from HomeController';
+    $scope.message = 'Hello from HomeController on github pages';
     console.log("page/home");
 }]);
