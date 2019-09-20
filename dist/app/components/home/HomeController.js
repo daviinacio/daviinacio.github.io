372e67b8e1c04dbf873623ca43f1b27419fa5ef2
daviinacio <aazz6850@gmail.com>
2019-09-20 10:49:11 -0300

    Top section

--- dist/app/components/home/HomeController.js
+++
@@ -1,4 +0,0 @@
-angular.module('myApp').controller('HomeController', ['$scope', '$location', function($scope, $location){
-    $scope.message = 'Hello from HomeController';
-    console.log("page/home");
-}]);
