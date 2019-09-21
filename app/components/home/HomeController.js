8fa5de34d39babec8d3343e3f5118ba5bd7f354c
daviinacio <aazz6850@gmail.com>
2019-09-21 00:43:22 -0300

    Top section style

--- app/components/home/HomeController.js
+++ app/components/home/HomeController.js
@@ -1,4 +1,8 @@
 angular.module('myApp').controller('HomeController', ['$scope', '$location', function($scope, $location){
     $scope.message = 'Hello from HomeController on github pages';
+    $scope.ondev = true;
     console.log("page/home");
+
+    //$('body').$scope().ondev = true;
+    //$('body').$scope().$apply();
 }]);
